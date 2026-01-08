d46cfcbdb71fd19e491953f86fbcfd823c376bf1
Rohit Raj <rohitrajptn2209@gmail.com>
2026-01-08 21:39:24 +0530

    8 jan

---
+++ 10.NUMPY QUESTIONS PRACTISE/1.EASY/easy.py
@@ -0,0 +1,5 @@
+# 1. A sensor records temperature every hour for 24 hours. 
+# Create a NumPy array of 24 values between 15 and 35 (inclusive).
+import numpy as np
+arr=np.fill(15,36,1)
+print(arr)
--- 2.CREATION/3.aranage.py
+++ 2.CREATION/3.aranage.py
@@ -4,5 +4,5 @@ arange(start,stop,step)
 print from "((((( start->stop-1 )))))"
 '''
 import numpy as np
-print(np.arange(1,100,1))
+print(np.arange(1,100,2))
 
