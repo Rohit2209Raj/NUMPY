d5455560c4b9d2258ff0bfcff0c8370857394a76
Rohit Raj <rohitrajptn2209@gmail.com>
2026-01-09 20:42:52 +0530

    9 jan

--- 10.NUMPY QUESTIONS PRACTISE/1.EASY/easy.py
+++ 10.NUMPY QUESTIONS PRACTISE/1.EASY/easy.py
@@ -122,51 +122,89 @@ import numpy as np
 # You have an array of exam scores.
 # Round all scores to the nearest multiple of 5.
 
-arr=np.array([10,12,23,43,23,67,89,0])
-# arr=np.where((arr % 5 <=2),arr-(arr % 5),arr+(5-(arr%5)))
-arr= (np.round(arr / 5)) * 5
-
-print(arr)
+# arr=np.array([10,12,23,43,23,67,89,0])
+# # arr=np.where((arr % 5 <=2),arr-(arr % 5),arr+(5-(arr%5)))
+# arr= (np.round(arr / 5)) * 5
+# print(arr)
 
 
 # 15.
 
-# Given an array of employee salaries, increase salaries by 10% for everyone earning less than 50,000.
+# # Given an array of employee salaries, increase salaries by 10% for everyone earning less than 50,000.
+# arr=np.array([56000,45000,67000,34000,55000,23000])
+# # arr=np.where(arr<50000,arr*1.10,arr)
+# arr = np.where(arr < 50000, np.round(arr * 1.10), arr).astype(int)
+# print(arr)
 
 # 16.
 
 # Create a 4×4 matrix of random integers between 1 and 20.
 # Find the maximum value in each row and each column.
+# matrix=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+# matrix=np.random.randint(1,21,size=(4,4))
+# print(matrix)
+# print(np.max(matrix,axis=0))
+# print(np.max(matrix,axis=1))
+
 
 # 17.
 
 # You have two arrays representing daily sales of two stores.
 # Compute the total sales per day by adding the arrays element-wise.
+# arr1=np.random.randint(1,100,10)
+# arr2=np.random.randint(1,100,10)
+# # print(arr1+arr2)
+# # Better method
+# arr_total=np.add(arr1,arr2)
+# print(arr_total)
 
 # 18.
 
 # Given an array of ages, count how many are between 18 and 30 inclusive.
+# arr=np.random.randint(1,100,40)
+# print(arr)
+# print(np.sum((arr>=18) & (arr<=30)))
 
 # 19.
 
-# You have a 2D array of marks.
-# Find the student (row) with the highest total marks.
+# # You have a 2D array of marks.
+# # Find the student (row) with the highest total marks.
+# matrix=np.array([[100,90,89],[98,78,88],[90,90,69]])
+# # print(np.max(np.sum(matrix,axis=1)))
+# arr_row_sum=np.sum(matrix,axis=1)
+# print(np.argmax(arr_row_sum)) 
+
 
 # 20.
 
 # Create an array of 15 random integers.
 # Sort the array without changing the original array.
+# arr=np.random.randint(1,100,15)
+# arr2=np.sort(arr)
+# print(arr2)
 
 # 21.
 
 # You have an array representing daily temperatures.
 # Convert all temperatures from Celsius to Fahrenheit.
 
-# 22.
+# celsius=np.array([32,34,45,21,12])
+# # fahrenheit=((celsius*(9/5))+32).astype(int)
+# fahrenheit = np.round((celsius * (9/5)) + 32).astype(int)
+# print(fahrenheit)
+# # 22.
 
 # Given a 1D array, reshape it into a 2D array with 3 columns (number of rows should adjust automatically).
+# arr=np.array([1,2,3,4,5,6,7,8,9])
+# arr=np.array([1,2,3,4,5,6])
+# matrix=arr.reshape(-1,3)
+# print(matrix)
 
 # 23.
 
 # You have an array with repeated values.
 # Find all unique values and their counts.
+arr=np.array([1,1,2,3,4,5,5,5,6,7,8])
+# arr2=np.unique(arr)
+arr2,count=np.unique(arr,return_counts=True)
+print(arr2,count)
---
+++ 10.NUMPY QUESTIONS PRACTISE/1.EASY/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+arr=np.random.randint(1,100,40)
+# print(arr)
+# print(np.sum((arr>=18) & (arr<=30)))
